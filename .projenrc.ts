61d41190375079f5ed70d50494064851eee0cdf8
Liam Johnston <liam.johnston@v2.digital>
2024-02-18 21:29:33 +1100

    feat(auto-release): first attempt at auto release (#5)

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,7 +1,7 @@
-import { IgnoreFile, Version } from 'projen';
+import { DockerCompose, IgnoreFile, Version } from 'projen';
 
 import { BunTypescript } from 'bun-ts-projen';
-import { GitlabCICDComponent } from './src';
+import { GitHubCICDComponent } from './src';
 
 const project = new BunTypescript({
   devDeps: [
@@ -23,11 +23,6 @@ const project = new BunTypescript({
 project.gitignore.addPatterns('.jsii', 'lib/', 'tsconfig.json');
 
 project.package.addField('jsii', {
-  typescript: {
-    compilerOptions: {
-      target: 'es2018',
-    },
-  },
   tsc: {
     outDir: 'lib',
     rootDir: 'src',
@@ -48,7 +43,7 @@ ignoreFile.addPatterns('node_modules/');
 
 project.makefile.addRule({
   targets: ['build'],
-  recipe: ['bun run build'],
+  recipe: ['docker compose run --rm app bun run build'],
 });
 
 new Version(project, {
@@ -58,7 +53,76 @@ new Version(project, {
 
 project.makefile.addRule({
   targets: ['bump'],
-  recipe: ['bun run bump']
+  recipe: ['docker compose run --rm node npm run bump'],
+});
+
+project.composeFile.addService('node', {
+  imageBuild: {
+    context: './containers',
+    dockerfile: 'Dockerfile.node',
+    args: {
+      NODE_VERSION: '20.11.1-alpine3.18',
+    },
+  },
+  environment: {
+    NPM_TOKEN: '${NPM_TOKEN:-}',
+  },
+  volumes: [DockerCompose.bindVolume('./', '/app')],
+});
+
+project.makefile.addRule({
+  targets: ['node_shell'],
+  recipe: ['docker compose run --rm node sh'],
+});
+
+project.makefile.addRule({
+  targets: ['commit_release'],
+  recipe: ['docker compose run --rm node make _commit_release'],
+});
+
+project.makefile.addRule({
+  targets: ['_commit_release'],
+  recipe: [
+    "git config --global user.name 'Github Actions'",
+    "git config --global user.email 'github@actions.com'",
+    "git commit -am 'chore(release)'",
+    'git tag $(cat ./lib/releasetag.txt)',
+    'git push --follow-tags',
+  ],
+});
+
+project.makefile.addRule({
+  targets: ['publish'],
+  recipe: ['docker compose run --rm node npm publish']
 })
+
+new GitHubCICDComponent(project, {
+  pushToMainWorkflowJobs: [
+    {
+      name: 'Build and Publish',
+      steps: [
+        {
+          name: 'Build',
+          commands: ['make build'],
+        },
+        {
+          name: 'Bump Version',
+          commands: ['make bump'],
+        },
+        {
+          name: 'Commit Release',
+          commands: ['make commit_release'],
+        },
+        {
+          name: 'Publish to NPM',
+          commands: ['make publish'],
+          environmentVariables: {
+            NPM_TOKEN: "$NPM_TOKEN"
+          }
+        },
+      ],
+    },
+  ],
+});
 
 project.synth();
